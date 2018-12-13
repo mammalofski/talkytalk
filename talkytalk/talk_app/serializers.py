58680301d359ba029f61fab8d627ca81cdd6425a
mammalofski <masahati.navid@gmail.com>
2018-12-13 23:50:28 +0330

    add some commenting

--- talkytalk/talk_app/serializers.py
+++ talkytalk/talk_app/serializers.py
@@ -15,10 +15,12 @@ class RoomSerializer(serializers.ModelSerializer):
         model = models.Room
 
     def create(self, validated_data):
+        # create the room
         room = models.Room.objects.create(
             callee=self.context['request'].user,
             room_id=validated_data.get('room_id'),
         )
+        # then add the callee himself/herself to participants
         room.participants.add(self.context['request'].user.id)
         return room
 
--- talkytalk/talk_app/views.py
+++ talkytalk/talk_app/views.py
@@ -27,8 +27,14 @@ class JoinRoom(generics.CreateAPIView):
     queryset = models.Room.objects.all()
 
     def create(self, request, *args, **kwargs):
+        # get the room
         room = get_object_or_404(models.Room, room_id=request.data.get('room_id'))
+        # add user to room participants
         room.participants.add(request.user.id)
+        # change room status to "on_call"
+        room.status = 2
+        room.save()
+        # serialize the data
         serializer = self.serializer_class(room)
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
@@ -40,5 +46,6 @@ class ListCreateContact(generics.ListCreateAPIView):
         return models.Contact.objects.filter(owner=self.request.user)
 
     def perform_create(self, serializer):
+        # save the contact with owner of the user
         serializer.save(owner=self.request.user)
 
