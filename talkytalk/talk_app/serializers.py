ccea338ab21329d942d62a55c207239bd2e9df8f
aliabdollahi <aliabdollahi0076p@gmail.com>
2018-12-14 17:32:55 +0330

    Add Django Channels and asgi.py and set EventSource in FrontEnd

--- talkytalk/talk_app/serializers.py
+++ talkytalk/talk_app/serializers.py
@@ -1,23 +1,39 @@
 from rest_framework import serializers
 from . import models
 
-# class RoomSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         fields = (
-#             'id',
-#             'name',
-#             'users',
-#         )
-#         model = models.Room
-#
-#
-# class MessageSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         fields = (
-#             'id',
-#             'sender',
-#             'room',
-#             'content',
-#             'created'
-#         )
-#         model = models.Message
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'callee',
+            'participants',
+            'created',
+            'status',
+            'room_id',
+        )
+        model = models.Room
+
+    def create(self, validated_data):
+        # create the room
+        room = models.Room.objects.create(
+            callee=self.context['request'].user,
+            room_id=validated_data.get('room_id'),
+        )
+        # then add the callee himself/herself to participants
+        room.participants.add(self.context['request'].user.id)
+        return room
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'owner',
+            'contact',
+            'contact_name',
+            'detail',
+            'created',
+        )
+        model = models.Contact
+
--- talkytalk/talk_app/urls.py
+++ talkytalk/talk_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path('room/', views.ListCreateRoom.as_view(), name='list_room'),
+    path('room/', views.ListCreateRoom.as_view(), name='list_room'),
+    path('room/<int:pk>', views.RetrieveUpdateDestroyRoom.as_view(), name='room_detail'),
+    path('room/join', views.JoinRoom.as_view(), name='join_room'),
+    path('contact', views.ListCreateContact.as_view(), name='list_contact'),
+    path('contact/<int:pk>', views.RetrieveUpdateDestroyContact.as_view(), name='contact_detail'),
 ]
